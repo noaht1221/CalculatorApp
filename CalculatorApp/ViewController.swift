cef35303d7794f4d381116cb348f9131495deb8c
noahthompson <net0016@auburn.edu>
2023-01-22 19:20:03 -0600

    Initial Commit

---
+++ CalculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by Noah Thompson on 1/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
